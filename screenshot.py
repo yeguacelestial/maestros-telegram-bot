7a6ee662f9898f0b48599db2bb3856b8986a83d6
Yegua Celestial <yeguacelestial@gmail.com>
2020-03-21 00:02:30 -0600

    Configured ChromeDriver for Heroku

--- screenshot.py
+++ screenshot.py
@@ -3,9 +3,12 @@ from selenium import webdriver
 
 options = webdriver.ChromeOptions()
 options.headless = True
+options.binary_location = GOOGLE_CHROME_BIN
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
 
 def capture(link):
-    with webdriver.Chrome(options=options) as driver:
+    with webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options) as driver:
         driver.get(link)
         time.sleep(3)
         image = driver.find_element_by_id('listado').screenshot('listado.png')
