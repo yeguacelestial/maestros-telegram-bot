3d71db4b372dc49d576d0a99acc70123ec4021b1
Yegua Celestial <yeguacelestial@gmail.com>
2020-03-20 23:36:44 -0600

    Changed from Firefox to Chromedriver

--- screenshot.py
+++ screenshot.py
@@ -1,11 +1,11 @@
 import time
 from selenium import webdriver
 
-options = webdriver.FirefoxOptions()
+options = webdriver.ChromeOptions()
 options.headless = True
 
 def capture(link):
-    with webdriver.Firefox(options=options) as driver:
+    with webdriver.Chrome(options=options) as driver:
         driver.get(link)
         time.sleep(3)
         image = driver.find_element_by_id('listado').screenshot('listado.png')
