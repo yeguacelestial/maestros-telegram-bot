1e09e165e70362ac70674c000c920b6adfc682ba
Yegua Celestial <yeguacelestial@gmail.com>
2020-03-21 11:25:59 -0600

    Modified firefoxdriver

--- screenshot.py
+++ screenshot.py
@@ -7,14 +7,16 @@ def capture(link):
     load_dotenv()
     FIREFOX_BIN = os.getenv('FIREFOX_BIN')
     GECKODRIVER_PATH = os.getenv('GECKODRIVER_PATH')
+    LD_LIBRARY_PATH = os.getenv('LD_LIBRARY_PATH')
+    PATH = os.getenv('PATH')
 
     options = webdriver.FirefoxOptions()
     options.headless = True
     options.binary_location = FIREFOX_BIN
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-gpu')
+    # options.add_argument('--no-sandbox')
 
-    with webdriver.Firefox(executable_path=GECKODRIVER_PATH, options=options) as driver:
+    with webdriver.Firefox(firefox_executable=PATH, options=options) as driver:
         driver.get(link)
         time.sleep(3)
         image = driver.find_element_by_id('listado').screenshot('listado.png')
